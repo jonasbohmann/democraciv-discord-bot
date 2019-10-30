adfb1ae62aec97484248803a44c31709042c694a
jonasbohmann <bohmannjonas@gmail.com>
2019-10-31 00:02:34 +0100

    Fix multiple PEP 8 violations

--- module/parties.py
+++ module/parties.py
@@ -22,21 +22,21 @@ class Party(commands.Cog, name='Political Parties'):
         return config.getPartyAliases().get(alias, alias)
 
     async def collectPartiesAndMembers(self, ctx):
-        partiesAndMembers = []
-        partyKeys = config.getParties().keys()
-        dcivGuild = self.bot.get_guild(int(config.getConfig()["homeServerID"]))
+        parties_and_members = []
+        party_keys = config.getParties().keys()
+        dciv_guild = self.bot.get_guild(int(config.getConfig()["homeServerID"]))
 
-        for party in partyKeys:
-            role = discord.utils.get(dcivGuild.roles, name=party)
+        for party in party_keys:
+            role = discord.utils.get(dciv_guild.roles, name=party)
 
             if role is None:
                 await ctx.send(f':x: "{party}" was added as a party but has '
                                f'no role on this server!')
                 continue
 
-            partiesAndMembers.append((party, len(role.members)))
+            parties_and_members.append((party, len(role.members)))
 
-        return partiesAndMembers
+        return parties_and_members
 
     @commands.command(name='join')
     @commands.cooldown(1, config.getCooldown(), commands.BucketType.user)
@@ -46,21 +46,21 @@ class Party(commands.Cog, name='Political Parties'):
             await ctx.send(':x: You have to give me a party as argument!')
             return
 
-        partyKeys = (config.getParties().keys())
+        party_keys = (config.getParties().keys())
         party = string.capwords(' '.join(party))
         party = self.getPartyFromAlias(party)
         member = ctx.message.author
         role = discord.utils.get(ctx.guild.roles, name=party)
 
         # Dict with party: partyLeader
-        inviteOnlyParties = {'المتنورين': 466851004290170902}
+        invite_only_parties = {'المتنورين': 466851004290170902}
 
         if party in config.getParties():
             if party not in [y.name for y in member.roles]:
-                if party in inviteOnlyParties:
-                    partyLeaderMention = self.bot.get_user(inviteOnlyParties[party])
+                if party in invite_only_parties:
+                    party_leader_mention = self.bot.get_user(invite_only_parties[party])
 
-                    msg = f':x: {party} is invite-only. Ask {partyLeaderMention.mention} for an invitation. '
+                    msg = f':x: {party} is invite-only. Ask {party_leader_mention.mention} for an invitation. '
                     await ctx.send(msg)
                     return
 
@@ -87,7 +87,6 @@ class Party(commands.Cog, name='Political Parties'):
                 embed.add_field(name='Member', value=member.mention + ' ' + member.name + '#' + member.discriminator,
                                 inline=False)
                 embed.add_field(name='Party', value=party)
-                embed.set_footer(text=config.getConfig()['botName'], icon_url=config.getConfig()['botIconURL'])
                 embed.timestamp = datetime.datetime.utcnow()
                 embed.set_thumbnail(url=member.avatar_url)
                 await logchannel.send(content=None, embed=embed)
@@ -95,7 +94,7 @@ class Party(commands.Cog, name='Political Parties'):
         elif party not in config.getParties():
             await ctx.send(':x: I could not find that party!\n\nTry one of these:')
             msg = ''
-            for key in partyKeys:
+            for key in party_keys:
                 msg += f'{key}\n'
             await ctx.send(msg)
 
@@ -116,7 +115,7 @@ class Party(commands.Cog, name='Political Parties'):
             return
 
         party = string.capwords(' '.join(party))
-        partyKeys = (config.getParties().keys())
+        party_keys = (config.getParties().keys())
 
         party = self.getPartyFromAlias(party)
 
@@ -143,7 +142,6 @@ class Party(commands.Cog, name='Political Parties'):
                 embed.add_field(name='Member', value=member.mention + ' ' + member.name + '#' + member.discriminator,
                                 inline=False)
                 embed.add_field(name='Party', value=party)
-                embed.set_footer(text=config.getConfig()['botName'], icon_url=config.getConfig()['botIconURL'])
                 embed.timestamp = datetime.datetime.utcnow()
                 embed.set_thumbnail(url=member.avatar_url)
                 await logchannel.send(content=None, embed=embed)
@@ -151,7 +149,7 @@ class Party(commands.Cog, name='Political Parties'):
         elif party not in config.getParties():
             await ctx.send(':x: I could not find that party!\n\nTry one of these:')
             msg = ''
-            for key in partyKeys:
+            for key in party_keys:
                 msg += f'{key}\n'
             await ctx.send(msg)
 
@@ -160,36 +158,36 @@ class Party(commands.Cog, name='Political Parties'):
     async def members(self, ctx, *party: str):
         """Lists all party members"""
 
-        dcivGuild = self.bot.get_guild(int(config.getConfig()["homeServerID"]))
+        dciv_guild = self.bot.get_guild(int(config.getConfig()["homeServerID"]))
 
-        if dcivGuild is None:
+        if dciv_guild is None:
             await ctx.send(':x: You have to invite me to the Democraciv server first!')
 
         if not party:
-            partyListEmbedContent = ''
+            party_list_embed_content = ''
 
-            sortedPartiesAndMembers = sorted(await self.collectPartiesAndMembers(ctx), key=lambda x: x[1], reverse=True)
+            sorted_parties_and_members = sorted(await self.collectPartiesAndMembers(ctx), key=lambda x: x[1], reverse=True)
 
-            for party in sortedPartiesAndMembers:
+            for party in sorted_parties_and_members:
                 if party[0] == 'Independent':
                     continue
                 if party[1] == 1:
-                    partyListEmbedContent += f'**{party[0]}**\n{party[1]} member\n\n'
+                    party_list_embed_content += f'**{party[0]}**\n{party[1]} member\n\n'
                 else:
-                    partyListEmbedContent += f'**{party[0]}**\n{party[1]} members\n\n'
+                    party_list_embed_content += f'**{party[0]}**\n{party[1]} members\n\n'
 
             # Append Independents to message
-            independentRole = discord.utils.get(dcivGuild.roles, name='Independent')
-            if len(independentRole.members) == 1:
-                partyListEmbedContent += f'⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n\n**Independent**\n{len(independentRole.members)} citizen' \
+            independent_role = discord.utils.get(dciv_guild.roles, name='Independent')
+            if len(independent_role.members) == 1:
+                party_list_embed_content += f'⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n\n**Independent**\n{len(independent_role.members)} citizen' \
                                          f'\n\n '
             else:
-                partyListEmbedContent += f'⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n\n**Independent**\n{len(independentRole.members)} citizen' \
+                party_list_embed_content += f'⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯⎯\n\n**Independent**\n{len(independent_role.members)} citizen' \
                                          f's\n\n'
 
             embed = embed_builder(title=f'Ranking of Political Parties in Arabia',
-                                  description=f'{partyListEmbedContent}', colour=0x7f0000)
-            embed.set_footer(text=config.getConfig()['botName'], icon_url=config.getConfig()['botIconURL'])
+                                  description=f'{party_list_embed_content}', colour=0x7f0000)
+            
             await ctx.send(embed=embed)
 
         elif party:
@@ -197,10 +195,10 @@ class Party(commands.Cog, name='Political Parties'):
 
             party = self.getPartyFromAlias(party)
 
-            role = discord.utils.get(dcivGuild.roles, name=party)
+            role = discord.utils.get(dciv_guild.roles, name=party)
 
             msg = ''
-            for member in dcivGuild.members:
+            for member in dciv_guild.members:
                 if role in member.roles:
                     msg += f'{member.name}\n'
             if msg == '':
@@ -217,7 +215,7 @@ class Party(commands.Cog, name='Political Parties'):
     @commands.command(name='addparty', hidden=True)
     @commands.cooldown(1, config.getCooldown(), commands.BucketType.user)
     @commands.has_permissions(administrator=True)
-    async def addparty(self, ctx, invite: str, *party: str):
+    async def addParty(self, ctx, invite: str, *party: str):
         """Add a new political party to the server"""
         if not party or not invite:
             await ctx.send(':x: You have to give me both the name and server invite of a political party to add!')
@@ -234,7 +232,7 @@ class Party(commands.Cog, name='Political Parties'):
     @commands.command(name='deleteparty', hidden=True)
     @commands.cooldown(1, config.getCooldown(), commands.BucketType.user)
     @commands.has_permissions(administrator=True)
-    async def deleteparty(self, ctx, *party: str):
+    async def deleteParty(self, ctx, *party: str):
         """Delete a political party from the server"""
         if not party:
             await ctx.send(':x: You have to give me the name of a political party to delete!')
@@ -251,12 +249,12 @@ class Party(commands.Cog, name='Political Parties'):
     @commands.command(name='addalias', hidden=True)
     @commands.cooldown(1, config.getCooldown(), commands.BucketType.user)
     @commands.has_permissions(administrator=True)
-    async def addalias(self, ctx, *partyAndAlias: str):
+    async def addAlias(self, ctx, *party_and_alias: str):
         """Adds a new alias to party"""
-        partyAndAlias: tuple = await self.getArguments(ctx, ' '.join(partyAndAlias), 2)
-        if partyAndAlias is None:
+        party_and_alias: tuple = await self.getArguments(ctx, ' '.join(party_and_alias), 2)
+        if party_and_alias is None:
             return
-        party, alias = partyAndAlias
+        party, alias = party_and_alias
         error = await config.addPartyAlias(party, alias)
 
         if error:
@@ -271,7 +269,7 @@ class Party(commands.Cog, name='Political Parties'):
     @commands.command(name='deletealias', hidden=True)
     @commands.cooldown(1, config.getCooldown(), commands.BucketType.user)
     @commands.has_permissions(administrator=True)
-    async def deletealias(self, ctx, *alias: str):
+    async def deleteAlias(self, ctx, *alias: str):
         """Deletes pre-existing alias"""
         alias = ' '.join(alias)
         error = await config.deletePartyAlias(alias)
@@ -285,40 +283,40 @@ class Party(commands.Cog, name='Political Parties'):
 
     @commands.command(name='listaliases')
     @commands.cooldown(1, config.getCooldown(), commands.BucketType.user)
-    async def listaliases(self, ctx, *party: str):
+    async def listAliases(self, ctx, *party: str):
         """Lists the given parties aliases, if any exist"""
         party = string.capwords(' '.join(party))
-        capsParty = party
+        caps_party = party
 
         parties, aliases = config.getParties(), config.getPartyAliases()
         if party in parties:
             pass
         elif party in aliases:
             party = aliases[party]
-            capsParty = string.capwords(party)
+            caps_party = string.capwords(party)
         else:
             await ctx.send(f':x: {party} not found!')
             return
 
         msg = ''
         for alias in aliases:
-            if aliases[alias] == party and alias != capsParty:
+            if aliases[alias] == party and alias != caps_party:
                 msg += f'{alias}\n'
 
         if msg:
             embed = embed_builder(title=f'Aliases of {party}', description=f'{msg}', colour=0x7f0000)
-            embed.set_footer(text=config.getConfig()['botName'], icon_url=config.getConfig()['botIconURL'])
+            
             await ctx.send(embed=embed)
         else:
             await ctx.send(f":x: No aliases found for {party}!")
 
-    async def getArguments(self, ctx, arguments: str, expectedArguments: int = -1):
+    async def getArguments(self, ctx, arguments: str, expected_arguments: int = -1):
         """Returns arguments split upon commas as a tuple of strings.
-        If arguments does not equal expectedArguments or there are blank arguments, posts a discord message and returns None.
-        If expectedArguments is -1, does not check for argument count."""
-        argumentCount = arguments.count(',') + 1
-        if expectedArguments != -1 and argumentCount != expectedArguments:
-            await ctx.send(f':x: Was given {argumentCount} arguments but expected {expectedArguments}!')
+        If arguments does not equal expected_arguments or there are blank arguments, posts a discord message and returns None.
+        If expected_arguments is -1, does not check for argument count."""
+        argument_count = arguments.count(',') + 1
+        if expected_arguments != -1 and argument_count != expected_arguments:
+            await ctx.send(f':x: Was given {argument_count} arguments but expected {expected_arguments}!')
             return None
 
         arguments = tuple(argument.strip() for argument in arguments.split(','))
