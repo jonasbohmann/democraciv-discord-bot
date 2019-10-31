9a0886e3dad9a3572a3f107effc81b148f4d3eee
jonasbohmann <bohmannjonas@gmail.com>
2019-10-31 13:39:11 +0100

    Add warnings to -addrole output for end user

--- module/roles.py
+++ module/roles.py
@@ -26,7 +26,7 @@ class Roles(commands.Cog):
         for role in available_roles:
             embed_message += f"{role}\n"
 
-        embed = embed_builder(title="Roles", description="To get a role, use `-role Role`")
+        embed = embed_builder(title="Roles", description="In order to add or remove a role from you, use `-role Role`")
         embed.add_field(name="Available Roles", value=embed_message)
         await ctx.send(embed=embed)
 
@@ -39,7 +39,7 @@ class Roles(commands.Cog):
             await ctx.send(":x: You have to tell me which role you want to join or leave!")
             return
 
-        role = ' '.join(role)
+        role = ''.join(role)
         member = ctx.message.author
         discord_role = discord.utils.get(ctx.guild.roles, name=role)
 
@@ -63,7 +63,8 @@ class Roles(commands.Cog):
         def check(message):
             return message.author == ctx.message.author and message.channel == ctx.message.channel
 
-        await ctx.send(":information_source: Answer with the name of the role you want to add: ")
+        await ctx.send(":information_source: Answer with the name of the role you want to add: \n\n:warning: The name "
+                       "should not contain *multiple* spaces between any word!")
         role_name = await self.bot.wait_for('message', check=check, timeout=60.0)
 
         await ctx.send(":information_source: Answer with a short message the user should see when he gets the role: ")
@@ -74,7 +75,8 @@ class Roles(commands.Cog):
         if error:
             await ctx.send(f':x: {error}')
         else:
-            await ctx.send(f':white_check_mark: Added {role_name.content} with "{role_join_message.content}"!')
+            await ctx.send(f':white_check_mark: Added the role "{role_name.content}" with the join message '
+                           f'"{role_join_message.content}"!')
 
     @commands.command(name='deleterole', hidden=True)
     @commands.cooldown(1, config.getCooldown(), commands.BucketType.user)
