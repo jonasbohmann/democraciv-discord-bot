f92ba02e89745278a8d4f2777d2ed943ec3da82f
jonasbohmann <bohmannjonas@gmail.com>
2019-11-14 00:30:48 +0100

    Remove redundant requirements

--- module/wikipedia.py
+++ module/wikipedia.py
@@ -9,12 +9,14 @@ class Wikipedia(commands.Cog):
         self.bot = bot
         wikipedia.set_lang('en')
 
+    # TODO - replace with aiohttp
+
     @commands.command(name='wikipedia')
     @commands.cooldown(1, config.getCooldown(), commands.BucketType.user)
     async def wikipedia(self, ctx, *, topic: str):
         """Search for a topic on Wikipedia\nUse quotes for topics that consist of multiple words!"""
         page = wikipedia.page(topic)
-        embed = self.bot.embeds.embed_builder(title=page.title, description=wikipedia.summary(topic, sentences=3),
+        embed = self.bot.embeds.embed_builder(title=page.title, description=wikipedia.summary(topic, sentences=2),
                                               colour=0x7f0000)
         embed.add_field(name='Link', value=page.url)
         await ctx.send(embed=embed)
