de176883edb3a40e223a2cc0a85de0bf376b20be
Jonas Bohmann <bohmannjonas@gmail.com>
2020-02-24 00:54:13 +0100

    Replace most link commands with tags

--- module/link.py
+++ module/link.py
@@ -61,34 +61,6 @@ class Link(commands.Cog):
         await ctx.send(embed=embed)
         return
 
-    @commands.command(name='parties', aliases=['p'])
-    @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
-    async def parties(self, ctx):
-        """Get a link to our Political Parties"""
-        embed = self.bot.embeds.embed_builder(title=f'Political Parties in {mk.NATION_NAME}', description=links.parties)
-        await ctx.send(embed=embed)
-
-    @commands.command(name='legalcode', aliases=['lc'])
-    @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
-    async def legalcode(self, ctx):
-        """Get a link to the Code of Law"""
-        embed = self.bot.embeds.embed_builder(title=f'Legal Code of {mk.NATION_NAME}', description=links.laws)
-        await ctx.send(embed=embed)
-
-    @commands.command(name='wiki', aliases=['w', 'info'])
-    @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
-    async def wiki(self, ctx):
-        """Check out the official Wiki"""
-        embed = self.bot.embeds.embed_builder(title='Official Wiki', description=links.wiki)
-        await ctx.send(embed=embed)
-
-    @commands.command(name='beginnersguide', aliases=['b'])
-    @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
-    async def beginnersGuide(self, ctx):
-        """Getting Started in Democraciv"""
-        embed = self.bot.embeds.embed_builder(title="Beginner's Guide", description=links.beginnersGuide)
-        await ctx.send(embed=embed)
-
     @commands.command(name='gamesessions', aliases=['gs'])
     @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
     async def gamesessions(self, ctx):
@@ -96,59 +68,6 @@ class Link(commands.Cog):
         embed = self.bot.embeds.embed_builder(title=f'Game Sessions with {mk.NATION_NAME}', description="")
         embed.add_field(name="List of Game Sessions on the Wiki", value=links.gswiki, inline=False)
         embed.add_field(name="YouTube Playlist", value=links.gameSessions, inline=False)
-        await ctx.send(embed=embed)
-
-    @commands.command(name='schedule')
-    @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
-    async def schedule(self, ctx):
-        """Schedule for the next elections"""
-        embed = self.bot.embeds.embed_builder(title='Schedule', description=links.schedule)
-        await ctx.send(embed=embed)
-
-    @commands.command(name='sue')
-    @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
-    async def sue(self, ctx):
-        """Submit a Case to the Supreme Court"""
-        embed = self.bot.embeds.embed_builder(title=f'Submit a Case to the Supreme Court of {mk.NATION_NAME}',
-                                              description=links.sue)
-        await ctx.send(embed=embed)
-
-    @commands.command(name='register')
-    @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
-    async def register(self, ctx):
-        """Register to Vote"""
-        embed = self.bot.embeds.embed_builder(title='Register to Vote', description=links.register)
-        await ctx.send(embed=embed)
-
-    @commands.command(name='states')
-    @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
-    async def states(self, ctx):
-        """Overview of our States"""
-        embed = self.bot.embeds.embed_builder(title=f'The States of {mk.NATION_NAME}', description=links.states)
-        await ctx.send(embed=embed)
-
-    @commands.command(name='docket', aliases=['d'])
-    @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
-    async def docket(self, ctx):
-        """Docket for the Legislature"""
-        embed = self.bot.embeds.embed_builder(title=f'Docket for the {mk.NATION_ADJECTIVE} Legislature',
-                                              description=links.legislativedocket)
-        await ctx.send(embed=embed)
-
-    @commands.command(name='worksheet')
-    @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
-    async def ministry(self, ctx):
-        """The Ministry's worksheet"""
-        embed = self.bot.embeds.embed_builder(title=f'Worksheet of the {mk.NATION_ADJECTIVE} Ministry',
-                                              description=links.executiveworksheet)
-        await ctx.send(embed=embed)
-
-    @commands.command(name='procedures', aliases=['procedure'])
-    @commands.cooldown(1, config.BOT_COMMAND_COOLDOWN, commands.BucketType.user)
-    async def legislativeprocedures(self, ctx):
-        """The Legislative Procedures"""
-        embed = self.bot.embeds.embed_builder(title=f'Procedures for the {mk.NATION_ADJECTIVE} Legislature',
-                                              description=links.legislativeprocedures)
         await ctx.send(embed=embed)
 
     @commands.command(name='invite')
