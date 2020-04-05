11d155b5b72f51010c1438bcc68083578bc68838
jonasbohmann <bohmannjonas@gmail.com>
2020-04-06 01:29:03 +0200

    Roll back -law search algorithm changes

--- module/democraciv/laws.py
+++ module/democraciv/laws.py
@@ -106,20 +106,23 @@ class Laws(commands.Cog, name='Law'):
 
         async with ctx.typing():
             # First, search by name
-            name_lookup = list({await self.bot.laws.search_law_by_name(' '.join(query))})
+            # results = await self.bot.laws.search_law_by_name(' '.join(query))
 
             # If the direct lookup by name didn't match anything, search for similar tag of each word of :param query
+            # if not results:
 
-            results = [name_lookup]
+            results = []
             for substring in query:
                 result = await self.bot.laws.search_law_by_tag(substring)
                 if result:
-                    result = list(set(result))
                     results.append(result)
 
             # As LawUtils.search_by_tag() returns a list of matches, put all elements of all sublists
             # into the results list
             results = [item for sublist in results for item in sublist]
+
+            # Eliminate duplicate results
+            results = list(set(results))
 
             if not results or results[0] == []:
                 results = ['Nothing found.']
