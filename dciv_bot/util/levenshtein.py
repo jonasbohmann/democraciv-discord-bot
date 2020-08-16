e1235979ef6015f50af9a5ba43b03589c94f61ca
The Great Cookie Machine <39784551+TheGreatCookieMachine@users.noreply.github.com>
2020-08-15 18:48:13 -0700

    Improve levenshtein speed and memory usage

--- dciv_bot/util/levenshtein.py
+++ dciv_bot/util/levenshtein.py
@@ -1,23 +1,17 @@
 def distance(a, b):
-    distances = [[0 for _ in range(len(b)+1)] for _ in range(len(a)+1)]
+    previous_distances = list(range(len(b)+1))
+    current_distances = [0 for _ in range(len(b)+1)]
 
     for i in range(len(a)):
-        distances[i+1][0] = i+1
-    
-    for j in range(len(b)):
-        distances[0][j+1] = j+1
-    
-    for i in range(len(a)):
+        current_distances[0] = i + 1
+
         for j in range(len(b)):
-            distances[i+1][j+1] = min(
-                distances[i][j+1] + 1,
-                distances[i+1][j] + 1,
-                distances[i][j] + (0 if a[i] == b[j] else 1)
+            current_distances[j+1] = min(
+                previous_distances[j+1] + 1,
+                current_distances[j] + 1,
+                previous_distances[j] + (0 if a[i] == b[j] else 1)
             )
-    
-    return distances[-1][-1]
 
-# Temprorary, for performance testing
-if __name__ == "__main__":
-    import timeit
-    print(timeit.timeit(stmt="distance('foo', 'barrr')", number=1000, globals=globals()))
+        previous_distances, current_distances = current_distances, previous_distances
+    
+    return previous_distances[-1]
