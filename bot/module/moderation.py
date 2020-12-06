ba809d2a58226307262c0f47bbf1e95f7ad255ee
Jonas Bohmann <bohmannjonas@gmail.com>
2020-12-06 07:29:30 +0100

    Fix docstring for Moderation cog

--- bot/module/moderation.py
+++ bot/module/moderation.py
@@ -10,7 +10,7 @@ from bot.utils.converter import UnbanConverter, BanConverter, CaseInsensitiveMem
 
 
 class Moderation(context.CustomCog):
-    """Commands for the Mod Team of this server and the Nation Admins"""
+    """Commands for the Mod Team of this server"""
 
     async def calculate_alt_chance(self, member: discord.Member) -> (int, str):
         is_alt_chance = 0
