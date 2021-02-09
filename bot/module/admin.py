08ccb49c2bc653d4eef19b11be9f7015608d8db2
Jonas Bohmann <bohmannjonas@gmail.com>
2021-02-09 20:15:09 +0100

    Add -jsk syncbill command

--- bot/module/admin.py
+++ bot/module/admin.py
@@ -67,6 +67,23 @@ class Admin(
         )
         await ctx.send(f"{config.YES} `{tag}` was added as a search tag to `{bill.name}` (#{bill.id})")
 
+    @commands.command(name="syncbill", aliases=["sb", 'synclaw', 'sl'])
+    @commands.is_owner()
+    async def syncbill(self, ctx, bills: commands.Greedy[models.Bill]):
+        """Refresh bill name and keywords from Google Docs"""
+
+        for bill in bills:
+            name, keywords = await bill.fetch_name_and_keywords()
+
+            await self.bot.db.execute("UPDATE bill SET name = $1 WHERE id = $2", name, bill.id)
+            await self.bot.db.execute("DELETE FROM bill_lookup_tag WHERE bill_id = $1", bill.id)
+
+            id_with_kws = [(bill.id, keyword) for keyword in keywords]
+            self.bot.loop.create_task(self.bot.db.executemany("INSERT INTO bill_lookup_tag (bill_id, tag) VALUES "
+                                                              "($1, $2) ON CONFLICT DO NOTHING ", id_with_kws))
+
+        await ctx.send(f"{config.YES} Synced {len(bills)} bills with Google Docs.")
+
 
 def setup(bot):
     bot.add_cog(Admin(bot))
