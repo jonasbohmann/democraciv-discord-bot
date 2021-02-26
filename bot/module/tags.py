a394064263311b52da524d961f078d3edd97c633
Jonas Bohmann <bohmannjonas@gmail.com>
2021-02-26 21:17:44 +0100

    Improve search results in -tag search

--- bot/module/tags.py
+++ bot/module/tags.py
@@ -590,10 +590,9 @@ class Tags(context.CustomCog):
 
         db_query = """SELECT tag.name, tag.title FROM tag
                       JOIN tag_lookup l on l.tag_id = tag.id
-                      WHERE 
-                      (tag.global = true AND (l.alias LIKE '%' || $1 || '%' OR tag.title LIKE '%' || $1 || '%'))
-                       OR 
-                      (tag.guild_id = $2 AND (l.alias LIKE '%' || $1 || '%' OR tag.title LIKE '%' || $1 || '%'))
+                      WHERE (tag.global = true OR tag.guild_id = $2)
+                      AND (l.alias % $1 OR l.alias LIKE '%' || $1 || '%' OR tag.title LIKE '%' || $1 || '%' 
+                          OR tag.content LIKE '%' || $1 || '%')
                       ORDER BY similarity(l.alias, $1) DESC
                       LIMIT 20
                     """
