0902aeec70bc3bd728a5fa949183b8e8533167d1
Jonas Bohmann <bohmannjonas@gmail.com>
2021-03-13 16:13:58 +0100

    Update -jsk syncbill to also fetch bill content

--- bot/module/admin.py
+++ bot/module/admin.py
@@ -70,12 +70,12 @@ class Admin(
     @commands.command(name="syncbill", aliases=["sb", "synclaw", "sl"])
     @commands.is_owner()
     async def syncbill(self, ctx, bills: commands.Greedy[models.Bill]):
-        """Refresh bill name and keywords from Google Docs"""
+        """Refresh bill name, content and keywords from Google Docs"""
 
         for bill in bills:
-            name, keywords = await bill.fetch_name_and_keywords()
+            name, keywords, content = await bill.fetch_name_and_keywords()
 
-            await self.bot.db.execute("UPDATE bill SET name = $1 WHERE id = $2", name, bill.id)
+            await self.bot.db.execute("UPDATE bill SET name = $1, content = $3 WHERE id = $2", name, bill.id, content)
             await self.bot.db.execute("DELETE FROM bill_lookup_tag WHERE bill_id = $1", bill.id)
 
             id_with_kws = [(bill.id, keyword) for keyword in keywords]
