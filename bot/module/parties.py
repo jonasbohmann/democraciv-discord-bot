ebda10ec7b52fcbb444ef86b4a31668f8e8d7bf3
Jonas Bohmann <bohmannjonas@gmail.com>
2021-03-21 22:52:55 +0100

    Adjust embed field layout in -parties

--- bot/module/parties.py
+++ bot/module/parties.py
@@ -480,7 +480,7 @@ class Party(context.CustomCog, name="Political Parties"):
             if independent_role:
                 embed.add_field(name="\u200b", value=f"**Independent**\n" f"{len(independent_role.members)} "
                                                      f"citizen{'s' if len(independent_role.members) > 1 else ''}",
-                                inline=True)
+                                inline=False)
 
         else:
             if sorted_parties_and_members:
